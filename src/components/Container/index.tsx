28274384776dfc3969be9697460da7e59e8e169d
Siddharth <147121225+siddhartheshkon@users.noreply.github.com>
2023-11-29 17:17:40 +0530

    + Added new Comp => Added Main Container  and layout for Task manager

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import {
+  BellOutlined,
+  DoubleLeftOutlined,
+  DoubleRightOutlined,
+  SettingOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, Button, theme, Typography } from "antd";
+
+import Style from "./index.module.scss";
+import ContentBox from "../ContentBox/Index";
+
+const { Sider, Content } = Layout;
+const { Title } = Typography;
+const Container: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout className={Style.Container}>
+      <Sider
+        trigger={
+          <Button
+            type="text"
+            icon={collapsed ? <DoubleRightOutlined /> : <DoubleLeftOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            className={Style.triggerBtn}
+          />
+        }
+        collapsible
+        collapsed={collapsed}
+      >
+        <Title level={2} color={"white"} className={Style.Title}>
+          TASKY
+        </Title>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "Dashboard",
+            },
+            {
+              key: "2",
+              icon: <BellOutlined />,
+              label: "Notification",
+              disabled: true,
+            },
+            {
+              key: "3",
+              icon: <SettingOutlined />,
+              label: "Setting",
+              disabled: true,
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Content className={Style.Content}>
+          <ContentBox />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Container;
